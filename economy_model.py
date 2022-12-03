b218d581c64b33bb1a427a82c28e3d5e83c0a94a
erissatallan <92621678+erissatallan@users.noreply.github.com>
2022-12-03 01:06:46 -0800

    Economy and Agent classes added

---
+++ economy_model.py
@@ -0,0 +1,14 @@
+
+import random, math, numpy as np
+
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.space import SingleGrid
+from mesa.datacollection import DataCollector
+
+# Global variables
+treasury = 0
+
+def incentive_gini():
+    # this method computes the degree of disparity in the reaction to incentives among agents in the economy
+    pass
